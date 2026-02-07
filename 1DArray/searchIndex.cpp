cc4e455bc0fb4e5d5910ef3d4253256518b133c7
Rafsan Rad <rafsanrad@Rafsans-MacBook-Air.local>
2026-02-07 23:50:29 +0600

    added search array problem

---
+++ 1DArray/searchIndex.cpp
@@ -0,0 +1,17 @@
+//Qs.Searching an element in the array.
+#include<iostream>
+using namespace std;
+int main(){
+    int mimo[10] = {32,4,5,12,5,54,6,23,3,5}; // declaration of a new array
+    int n, i;
+    cout<<"Enter the number to be searched: "<<endl;
+    cin>>n; // inputting the number to be searched in the array
+    for(i=0; i<10; i++){ // searching begins
+	if (n == mimo[i]){
+	  break; // searching ends
+	}
+    }
+    cout<<n<<" was found in index "<<i<<" of the array."<<endl;
+
+    return 0;
+}
