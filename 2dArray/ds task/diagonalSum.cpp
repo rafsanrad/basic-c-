3b44cbfe7ba48222bf3a63723db5f9976e8416f8
Rafsan Rad <rafsanrad@Rafsans-MacBook-Air.local>
2026-02-10 14:05:02 +0600

    added diagonal matrix problem

---
+++ 2dArray/ds task/diagonalSum.cpp
@@ -0,0 +1,33 @@
+// 2. Write a Program in C++ to take a NXN Square matrix input from user. Now find the sum of all the diagonal elements. Make sure to consider all values only once.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of square matrix: ";
+    cin >> n;
+
+    int matrix[n][n];
+    int sum = 0;
+
+    cout << "Enter matrix elements:\n";
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    // Diagonal sum
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(i == j || i + j == n - 1) {
+                sum += matrix[i][j];
+            }
+        }
+    }
+    cout << "Sum of diagonal elements = " << sum << endl;
+
+    return 0;
+}
+
